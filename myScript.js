06260f12d595edf5f47f871ee15ebe7ee3a2f423
Nischal Atreya <71140529+Ed-A@users.noreply.github.com>
2024-05-31 15:55:54 -0400

    updated Knight
    added graphics

--- myScript.js
+++ myScript.js
@@ -1,52 +1,57 @@
 let piece = "";
+let piecealt = "";
 let index = "";
 
 class Knight{
-  constructor(i,j){
-    this.i = i;
-    this.j = j;
-  }
+  // CLASS OF THE PIECE WITH METHOD THAT CALCULATES POSSIBLE MOVES
   lightSquares(i,j) {
-    if(i+1>=0 && i+1<=4 && j+2 <=4 && j+2 >=0){document.getElementById((i+1) + "_" + (j +2)).innerHTML = "O";}
-    if(i+2>=0 && i+2<=4 && j+1 <=4 && j+1 >=0){document.getElementById((i+2) + "_" + (j+1)).innerHTML = "O";}
-    if(i-1>=0 && i-1<=4 && j-2 <=4 && j-2 >=0){document.getElementById((i-1) + "_" + (j-2)).innerHTML = "O";}
-    if(i-2>=0 && i-2<=4 && j-1 <=4 && j-1 >=0){document.getElementById((i-2) + "_" + (j-1)).innerHTML = "O";}
-    if(i-1>=0 && i-1<=4 && j+2 <=4 && j+2 >=0){document.getElementById((i-1) + "_" + (j +2)).innerHTML = "O";}
-    if(i+2>=0 && i+2<=4 && j-1 <=4 && j-1 >=0){document.getElementById((i+2) + "_" + (j-1)).innerHTML = "O";}
-    if(i+1>=0 && i+1<=4 && j-2 <=4 && j-2 >=0){document.getElementById((i+1) + "_" + (j-2)).innerHTML = "O";}
-    if(i-2>=0 && i-2<=4 && j+1 <=4 && j+1 >=0){document.getElementById((i-2) + "_" + (j+1)).innerHTML = "O";}
-    piece = "Kn";
+    document.getElementById(i+"_"+j).style.backgroundColor = "green";
+    if(i+1>=0 && i+1<=4 && j+2 <=4 && j+2 >=0){document.getElementById((i+1) + "_" + (j+2)).style.backgroundColor = "orange";}
+    if(i+2>=0 && i+2<=4 && j+1 <=4 && j+1 >=0){document.getElementById((i+2) + "_" + (j+1)).style.backgroundColor = "orange";}
+    if(i-1>=0 && i-1<=4 && j-2 <=4 && j-2 >=0){document.getElementById((i-1) + "_" + (j-2)).style.backgroundColor = "orange";}
+    if(i-2>=0 && i-2<=4 && j-1 <=4 && j-1 >=0){document.getElementById((i-2) + "_" + (j-1)).style.backgroundColor = "orange";}
+    if(i-1>=0 && i-1<=4 && j+2 <=4 && j+2 >=0){document.getElementById((i-1) + "_" + (j+2)).style.backgroundColor = "orange";}
+    if(i+2>=0 && i+2<=4 && j-1 <=4 && j-1 >=0){document.getElementById((i+2) + "_" + (j-1)).style.backgroundColor = "orange";}
+    if(i+1>=0 && i+1<=4 && j-2 <=4 && j-2 >=0){document.getElementById((i+1) + "_" + (j-2)).style.backgroundColor = "orange";}
+    if(i-2>=0 && i-2<=4 && j+1 <=4 && j+1 >=0){document.getElementById((i-2) + "_" + (j+1)).style.backgroundColor = "orange";}
+    piece = "whiteknight.png";
+    piecealt = "whiteknight";
     index = i + "_" + j;
   }
 }
 
 function check(a, i, j){
-  switch (a){
-    case 'Kn':
-      K1 = new Knight;
-      K1.lightSquares(i, j);
-      break;
-    case 'O':
-      let elements = document.getElementsByTagName('td');
-      for(let count = 0; count < elements.length; count++)
-      {
-          if(elements[count].innerHTML == "O" || elements[count].innerHTML == piece)
-          {
-              elements[count].innerHTML = "";
-          }
-      }
-      document.getElementById(i + "_" + j).innerHTML = piece;
-      break;
-    case '':
-      let all = document.getElementsByTagName('td');
-      for(let count = 0; count < all.length; count++)
-      {
-          if(all[count].innerHTML == "O")
-          {
-              all[count].innerHTML = "";
-          }
-      }
-      piece = "";
-      index = "";
+  // FUNCTION TO CHECK CLICKS AND RESPOND ACCORDINGLY
+  if(a.style.backgroundColor != "orange"){
+    switch (a.children[0].alt){
+      case 'whiteknight':
+        K1 = new Knight;
+        K1.lightSquares(i, j);
+        break;
+      case '':
+        remove();
+        piece = "";
+        index = "";
+    }
+  }else{
+    remove();
+    // CHANGES PIECE POSITION
+    a.children[0].src = piece;
+    a.children[0].alt = piecealt;
+    document.getElementById(index).children[0].src = "clear.png";
+    document.getElementById(index).children[0].alt = "";
+    document.getElementById(index).style.backgroundColor = "transparent";
   }
+}
+
+function remove(){
+  // REMOVES POSSIBLE MOVES COLOR
+  let elements = document.getElementsByTagName('td');
+    for(let count = 0; count < elements.length; count++)
+    {
+        if(elements[count].style.backgroundColor == "orange")
+        {
+            elements[count].style.backgroundColor = "transparent";
+        }
+    }
 }
